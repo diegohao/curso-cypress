2d90c9b4ea14956bdfa8a457185213dd24098369
Diego Henrique <diegobr@hotmail.com>
2021-03-03 09:04:37 -0300

    Inclusão do cenário Alterar Conta

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -32,6 +32,24 @@ describe('Should test at a functional level', () => {
     })
 
     it('Should update an account', () => {
+        cy.request({
+            url: '/contas',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}` },
+            qs: {
+                nome: 'Conta para alterar'
+            }
+        }).then(res => {
+            cy.request({
+                url: `https://barrigarest.wcaquino.me/contas/${res.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    nome: 'conta alterada via rest'
+                }
+            }).as('response')
+        })
+        cy.get('@response').its('status').should('be.equal', 200)
     })
 
     it('Should not create an account with same name', () => {
