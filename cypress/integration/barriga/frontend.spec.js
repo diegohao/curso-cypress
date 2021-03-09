c2be14bc8726a1b781267de8167216c832e45b8c
Diego Henrique <diegobr@hotmail.com>
2021-03-09 09:15:24 -0300

    Inclusão do cenário de incluir conta

--- cypress/integration/barriga/frontend.spec.js
+++ cypress/integration/barriga/frontend.spec.js
@@ -4,6 +4,10 @@ import loc from '../../support/locators'
 import '../../support/commandsContas'
 
 describe('Should test at a frontend level', () => {
+    after(() => {
+      cy.clearLocalStorage()
+    })
+
     before(() => {
       cy.server()
       cy.route({
@@ -35,9 +39,38 @@ describe('Should test at a frontend level', () => {
     })
     
     beforeEach(() => {
+      cy.get(loc.MENU.HOME).click()
     })
 
-    it('Should create an account', () => {
+    it.only('Should create an account', () => {
+      cy.route({
+        method: 'GET',
+        url: '/contas',
+        response: [
+          { id: 1, nome: 'Carteira', visivel: true, usuario_id: 1 },
+          { id: 2, nome: 'Banco', visivel: true, usuario_id: 1 }
+        ]
+      }).as('Contas')
+
+      cy.route({
+        method: 'POST',
+        url: '/contas',
+        response: { id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1 }
+      }).as('saveConta')
+
+      cy.acessarMenuConta()
+
+      cy.route({
+        method: 'GET',
+        url: '/contas',
+        response: [
+          { id: 1, nome: 'Carteira', visivel: true, usuario_id: 1 },
+          { id: 2, nome: 'Banco', visivel: true, usuario_id: 1 },
+          { id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1 }
+        ]
+      }).as('contasSaved')
+
+      cy.inserirConta('Conta de teste')
     })
 
     it('Should update an account', () => {
